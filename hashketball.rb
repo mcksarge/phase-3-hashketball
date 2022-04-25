5e2d8cbdfa01da95ca6b6d3dc6a486477d35c35f
mcksarge <higgins.fred.s@gmail.com>
2022-04-25 10:19:56 -0700

    completed

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,7 @@
 # Write your code below game_hash
+require 'pry'
+
+
 def game_hash
   {
     home: {
@@ -126,4 +129,131 @@ def game_hash
   }
 end
 
-# Write code here
+#binding.pry
+
+#Helper method
+def all_teams
+  puts game_hash[:home][:team_name]
+  puts game_hash[:away][:team_name]
+end
+
+def home_team
+  game_hash[:home]
+end
+
+def away_team
+  game_hash[:away]
+end
+
+#Return players points scored
+def num_points_scored(name)
+  score = 0
+
+  away_team[:players].each do |player|
+    if player[:player_name] == name
+      score = player[:points]
+    end
+  end
+
+  home_team[:players].each do |player|
+    if player[:player_name] == name
+      score = player[:points]
+    end
+  end
+  score
+end
+
+#Returns player shoe size
+def shoe_size(name)
+  size = 0
+
+  away_team[:players].each do |player|
+    if player[:player_name] == name
+      size = player[:shoe]
+    end
+  end
+
+  home_team[:players].each do |player|
+    if player[:player_name] == name
+      size = player[:shoe]
+    end
+  end
+  size
+end
+
+def team_colors(team)
+  colors = ""
+  if away_team[:team_name] == team
+    colors = away_team[:colors]
+  elsif home_team[:team_name] == team
+    colors = home_team[:colors]
+  end
+  colors
+end
+
+#Returns team names
+def team_names
+    teams = []
+
+    teams << away_team[:team_name]
+    teams << home_team[:team_name]
+
+    teams
+end
+
+#Returns the players jersey numbers
+def player_numbers(team)
+  numbers = []
+
+  if away_team[:team_name] == team
+    away_team[:players].each do |player|
+      numbers << player[:number]
+    end
+
+  elsif home_team[:team_name] == team
+    home_team[:players].each do |player|
+      numbers << player[:number]
+    end
+  end
+  numbers
+end
+
+#Returns player stats
+def player_stats(name)
+  stats = ""
+  away_team[:players].each do |player|
+    if player[:player_name] == name
+      stats = player
+    end
+  end
+
+  home_team[:players].each do |player|
+    if player[:player_name] == name
+      stats = player
+    end
+  end
+  stats
+end
+
+#Returns number of rebounds of player with biggest shoe size
+def big_shoe_rebounds
+  shoe_size = 0
+  rebounds = 0
+
+  away_team[:players].each do |player|
+    if player[:shoe] > shoe_size
+      shoe_size = player[:shoe]
+      rebounds = player[:rebounds]
+    end
+  end
+
+  home_team[:players].each do |player|
+    if player[:shoe] > shoe_size
+      shoe_size = player[:shoe]
+      rebounds = player[:rebounds]
+    end
+  end
+
+  rebounds
+
+end
